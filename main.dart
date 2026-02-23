89742058e1ac3931cdc2b7e14e7374cd636b7d04
Junaet Hasan <junaethasanmiju@gmail.com>
2026-02-23 14:49:41 +0600

    Map add

--- main.dart
+++ main.dart
@@ -1,64 +1,20 @@
+import 'map.dart';
+
 void main (){
-
-
-List userList = [
-
-  'Miju','Leyon','Nabil',
-  
-];
-print(userList);
-
-print(userList.length);
-userList.add('Nab');
-print(userList);
-userList.remove('Miju');
-print(userList);
-userList.addAll(['Mihad','Hridoy']);
-print(userList);
-userList.map((userLisrt) =>userLisrt.name.toString());
-print(userList);
-userList.removeAt(2);
-
-print(userList);  
-print(userList);
-userList.setAll(0, ['item add']);
-print(userList);
-print(userList.isEmpty);
-print(userList.isNotEmpty);
-
-
-List <String> userLitte = [
-  
-  'Miju','Leyon','Nabil',
-  
-];
-print(userLitte);
-
-print(userLitte.length);
-userLitte.add('Nab');
-print(userLitte);
-userLitte.remove('Miju');
-print(userLitte);
-userLitte.addAll(['Mihad','Hridoy']);
-print(userLitte);
-//userLitte.map((userLisrt) =>userLisrt.name.toString());
-print(userLitte);
-userLitte.removeAt(2);
-
-print(userLitte);  
-print(userLitte);
-userLitte.setAll(0, ['item add']);
-print(userLitte);
-print(userLitte.isEmpty);
-print(userLitte.isNotEmpty);
-
-
-List<int> list = [
-  1,2,3,5,6,7,8,
-];
-print(list);
-list.add(89);
-print(list);
-
-
+  print(user);
+  print(user.isEmpty);
+  print(user.isNotEmpty);
+  print(user.keys);
+  print(user.length);
+  print(user.remove(2)); 
+  print(user.runtimeType);
+  print(user.values);
+  user.addAll({1: 'Tonmoy',2:'Shathi'});
+  print(user);
+  user.clear();
+  print(user);
+  user.remove({'Tonni'});
+  print(user);
+  user.toString();
+  print(user);
 }
---
+++ map.dart
@@ -0,0 +1,12 @@
+Map<int,String> user = {
+
+1:'Tonni',
+2:'Mihad',
+3:'Habib',
+4:'ddd'
+
+
+};
+Map<String,dynamic> userList = {
+  
+};
